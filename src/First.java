18e2499d827b8ce5a27c690331250b7ef371b20d
Pfalky <pfalky@yandex.ru>
2016-02-24 02:01:37 +0300

    Initial commit

---
+++ src/First.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+/**
+ * Created by Root on 23.02.2016.
+ */
+public class First {
+    private JTextPane  TextPane;
+    private JRadioButton radioButton1;
+    private JCheckBox a3CheckBox;
+    private JCheckBox a2CheckBox;
+    private JCheckBox a1CheckBox;
+    private JButton  Button;
+    private JFormattedTextField formattedTextField1;
+    private JPanel panel1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
